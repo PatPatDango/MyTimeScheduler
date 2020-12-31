bf15e5b5cfa61f162163e0ce768f810b96e1a59d
joshu <joshu@192.168.0.248>
2020-12-31 13:57:37 +0100

    Added Classes

--- src/Classes/Appointment.java
+++ src/Classes/Appointment.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mytimescheduler;
+package Classes;
 
 /**
  *
  * @author joshu
  */
-public class MyTimeScheduler {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Appointment {
     
 }
